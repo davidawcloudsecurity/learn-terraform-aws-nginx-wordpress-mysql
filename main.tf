b0c6699fc27faffb062b7ef18a494ad6d4e5d1be
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-08-24 23:00:42 +0800

    Update main.tf - configure default.conf

--- main.tf
+++ main.tf
@@ -218,10 +218,10 @@ resource "aws_instance" "nginx" {
 
                   location / {
                       proxy_pass http://${aws_instance.wordpress.private_ip};
-                      proxy_set_header Host \$host;
-                      proxy_set_header X-Real-IP \$remote_addr;
-                      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-                      proxy_set_header X-Forwarded-Proto \$scheme;
+                      proxy_set_header Host $host;
+                      proxy_set_header X-Real-IP $remote_addr;
+                      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                      proxy_set_header X-Forwarded-Proto $scheme;
                   }
               }' > ./default.conf
 
